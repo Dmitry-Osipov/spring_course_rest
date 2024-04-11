aa45aa3d26622f33eb6fbced7b4b26f27722ab13
Dmitry Osipov <dimaosipov00@gmail.com>
2024-04-11 18:07:00 +0300

    Added save entity

--- src/main/java/com/osipov/spring/mvc/controller/MyRestController.java
+++ src/main/java/com/osipov/spring/mvc/controller/MyRestController.java
@@ -10,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +40,12 @@ public class MyRestController {
             throw new NoSuchEmployeeException("There is no employee with id " + id + " in database");
         }
 
+        return ResponseEntity.ok(employee);
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
         return ResponseEntity.ok(employee);
     }
 }
